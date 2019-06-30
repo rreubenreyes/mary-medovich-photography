dedf859dc62d7ed2fa1ad7aeb66fe550eb3f63f7
Reuben Reyes <reuben.a.reyes@gmail.com>
2019-06-30 13:28:37 -0700

    add blog post route on compile

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,36 +1,48 @@
-// const path = require('path');
+const path = require('path');
+const slug = require('slug');
 
-// const createPages = await ({ graphql, actions }) => {
-//   const { createPage } = actions;
-//   const blogPostTemplate = path.resolve('src/templates/blog-post.js');
-//   // Query for markdown nodes to use in creating pages.
-//   // You can query for whatever data you want to create pages for e.g.
-//   // products, portfolio items, landing pages, etc.
-//   // Variables can be added as the second function parameter
-//   const pages = graphql(`
-//     query loadPagesQuery ($limit: Int!) {
-//       allMarkdownRemark(limit: $limit) {
-//         edges {
-//           node {
-//             frontmatter {
-//               slug
-//             }
-//           }
-//         }
-//       }
-//     }
-//   `, { limit: 1000 })
+const createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+    const blogPostTemplate = require.resolve(
+        path.join(__dirname, './src/templates/blog-post.js')
+    );
+    // Query for markdown nodes to use in creating pages.
+    // You can query for whatever data you want to create pages for e.g.
+    // products, portfolio items, landing pages, etc.
+    // Variables can be added as the second function parameter
+    const pages = await graphql(`
+        query loadPagesQuery {
+            gcms {
+                blogPosts(orderBy: createdAt_DESC) {
+                    title
+                    content {
+                        markdown
+                    }
+                    createdAt
+                    updatedAt
+                }
+                tags {
+                    name
+                }
+            }
+        }
+    `);
 
-//     if (pages.errors) {
-//         throw pages.errors;
-//     }
+    if (pages.errors) {
+        throw pages.errors;
+    }
 
-//     pages.data.gcms.forEach(edge => {
-//         creaePage({
-//             path: `${edg}`
-//         })
-//     })
-// };
-// module.exports = {
-//     createPages,
-// };
+    pages.data.gcms.blogPosts.forEach(blogPost => {
+        createPage({
+            path: slug(blogPost.title, { lower: true }),
+            component: blogPostTemplate,
+            context: {
+                ...blogPost,
+            },
+        });
+    });
+};
+
+module.exports = {
+    createPages,
+};
