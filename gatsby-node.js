9e9438627004a2262ea4660f2bfb243abf20f639
Reuben Reyes <reuben.a.reyes@gmail.com>
2019-06-30 12:15:03 -0700

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,36 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const path = require('path');
 
-// You can delete this file if you're not using it
+// const createPages = await ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const blogPostTemplate = path.resolve('src/templates/blog-post.js');
+//   // Query for markdown nodes to use in creating pages.
+//   // You can query for whatever data you want to create pages for e.g.
+//   // products, portfolio items, landing pages, etc.
+//   // Variables can be added as the second function parameter
+//   const pages = graphql(`
+//     query loadPagesQuery ($limit: Int!) {
+//       allMarkdownRemark(limit: $limit) {
+//         edges {
+//           node {
+//             frontmatter {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `, { limit: 1000 })
+
+//     if (pages.errors) {
+//         throw pages.errors;
+//     }
+
+//     pages.data.gcms.forEach(edge => {
+//         creaePage({
+//             path: `${edg}`
+//         })
+//     })
+// };
+// module.exports = {
+//     createPages,
+// };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,101 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const MockPhotoComponent = ({ photo }) => {
+    const {
+ title, location, description, tags,
+} = photo;
 
-export default IndexPage
+    return (
+        <div className="homepage__div--photo-container">
+            <p>{title}</p>
+            <p>
+                {location.latitude}
+{' '}
+{location.longitude}
+            </p>
+            <p>{description.markdown}</p>
+            <p>{tags.name}</p>
+        </div>
+    );
+};
+
+MockPhotoComponent.propTypes = {
+    photo: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        location: PropTypes.shape({
+            latitude: PropTypes.number.isRequired,
+            longitude: PropTypes.number.isRequired,
+        }),
+        description: PropTypes.shape({
+            markdown: PropTypes.string.isRequired,
+        }),
+        tags: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+        }),
+    }).isRequired,
+};
+
+const IndexPage = ({ data }) => {
+    const {
+        gcms: { photos },
+    } = data;
+
+    return (
+        <Layout>
+            <SEO title="Home" />
+            <h1>Hi people</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+                <Image />
+            </div>
+            <div className="homepage__div--photos-container">
+                {photos.map(photo => (
+                    <MockPhotoComponent key={photo.id} photo={photo} />
+                ))}
+            </div>
+
+            <Link to="/page-2/">Go to page 2</Link>
+        </Layout>
+    );
+};
+
+export const allPhotosQuery = graphql`
+    query MyQuery {
+        gcms {
+            photos(orderBy: updatedAt_DESC) {
+                title
+                location {
+                    latitude
+                    longitude
+                }
+                description {
+                    markdown
+                }
+                tags {
+                    name
+                }
+                description {
+                    raw
+                    html
+                    markdown
+                    text
+                }
+            }
+        }
+    }
+`;
+
+IndexPage.propTypes = {
+    data: PropTypes.shape({
+        gcms: PropTypes.object.isRequired,
+    }).isRequired,
+};
+
+export default IndexPage;
